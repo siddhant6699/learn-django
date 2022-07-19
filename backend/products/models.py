0aefabdef58bfa60ccc4305d89b790ddd6a44609
siddhant6699 <sidroxx.sc@gmail.com>
2022-07-19 20:40:34 +0530

    used the model to show in the view responce

--- backend/products/models.py
+++ backend/products/models.py
@@ -6,5 +6,5 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    contect = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
